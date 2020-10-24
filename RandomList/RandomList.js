2eabaef93e42ca417bb9372336592d04a45f51e9
ganbaru00000 <ganbaru00000@gmail.com>
2020-10-24 22:07:33 +0900

    randomlist

---
+++ RandomList/RandomList.js
@@ -0,0 +1,13 @@
+function RandomList(min, max, length) {
+  //0 < min < maxかつ整数であること
+  //length 整数であること
+  let rList = new Array();
+
+  for(let i = 0; i < length; i++){
+    rList.push(Math.floor(Math.random() * ((max + 1) - min)) + min); 
+  }
+  
+  console.log(rList);
+  return rList;
+
+}
